6146b3a28ef5e374d2f20e44def1d104d220fa98
EDHMKY <edhm01@gmail.com>
2021-12-06 15:07:30 -0500

    Completed Literal Math Assignment. I am not sure what happened to it.

---
+++ script.js
@@ -0,0 +1,45 @@
+
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+
+
+console.log(warmHugs.toUpperCase());
+
+
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs);
+
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+
+console.log(beenImpaled.slice(18));
+
+let dotDotDot = "...";
+
+
+let skullBones = `I don't have a skull${dotDotDot}or bones.`;
+console.log(skullBones);
+
+
+console.log(Math.PI);
+
+
+let randomNum = Math.random();
+randomNum *= 3;
+randomNum = Math.floor(randomNum);
+randomNum++;
+console.log(randomNum);
+
+
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+
+
+let reindeers = "Reindeers are better than people.";
+
+
+console.log(reindeers.replace(/ /g, "_"));
+
+console.log(Math.SQRT2);
+
+
+
